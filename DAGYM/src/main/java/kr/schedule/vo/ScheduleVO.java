85fd7f793495ade8f9bb8936ddcaa87deeefac8d
songhyogi <thdhk1230@gmail.com>
2024-05-23 15:39:55 +0900

    스케줄 자바빈

--- DAGYM/src/main/java/kr/schedule/vo/ScheduleVO.java
+++ DAGYM/src/main/java/kr/schedule/vo/ScheduleVO.java
@@ -1,5 +1,16 @@
 package kr.schedule.vo;
 
 public class ScheduleVO {
-
+	private int sch_num;
+	private int mem_num;
+	private String sch_date;
+	private int sch_status;
+	private int his_num;
+	private int tra_num;
+	private int his_status;
+	private String his_part;
+	private int pay_num;
+	private int pay_fee;
+	private int pay_enroll;
+	//
 }
