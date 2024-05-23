a954d15f344ba25374bb8cf0cef0515fd6c43856
songhyogi <thdhk1230@gmail.com>
2024-05-23 15:49:19 +0900

    스케줄 DAO

--- DAGYM/src/main/java/kr/schedule/vo/ScheduleVO.java
+++ DAGYM/src/main/java/kr/schedule/vo/ScheduleVO.java
@@ -12,5 +12,71 @@ public class ScheduleVO {
 	private int pay_num;
 	private int pay_fee;
 	private int pay_enroll;
-	//
+	public int getSch_num() {
+		return sch_num;
+	}
+	public void setSch_num(int sch_num) {
+		this.sch_num = sch_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public String getSch_date() {
+		return sch_date;
+	}
+	public void setSch_date(String sch_date) {
+		this.sch_date = sch_date;
+	}
+	public int getSch_status() {
+		return sch_status;
+	}
+	public void setSch_status(int sch_status) {
+		this.sch_status = sch_status;
+	}
+	public int getHis_num() {
+		return his_num;
+	}
+	public void setHis_num(int his_num) {
+		this.his_num = his_num;
+	}
+	public int getTra_num() {
+		return tra_num;
+	}
+	public void setTra_num(int tra_num) {
+		this.tra_num = tra_num;
+	}
+	public int getHis_status() {
+		return his_status;
+	}
+	public void setHis_status(int his_status) {
+		this.his_status = his_status;
+	}
+	public String getHis_part() {
+		return his_part;
+	}
+	public void setHis_part(String his_part) {
+		this.his_part = his_part;
+	}
+	public int getPay_num() {
+		return pay_num;
+	}
+	public void setPay_num(int pay_num) {
+		this.pay_num = pay_num;
+	}
+	public int getPay_fee() {
+		return pay_fee;
+	}
+	public void setPay_fee(int pay_fee) {
+		this.pay_fee = pay_fee;
+	}
+	public int getPay_enroll() {
+		return pay_enroll;
+	}
+	public void setPay_enroll(int pay_enroll) {
+		this.pay_enroll = pay_enroll;
+	}
+	
 }
