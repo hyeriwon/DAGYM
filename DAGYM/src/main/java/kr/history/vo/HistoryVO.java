0de029299cbaf2cbd5036fe8520cf278bbd99b1a
songhyogi <thdhk1230@gmail.com>
2024-05-23 17:19:06 +0900

    [ADD] HistoyVO, PaymentVO [UPDATE] ScheduleDAO

---
+++ DAGYM/src/main/java/kr/history/vo/HistoryVO.java
@@ -0,0 +1,48 @@
+package kr.history.vo;
+
+public class HistoryVO {
+	private int his_num;
+	private int mem_num;
+	private int sch_num;
+	private int tra_num;
+	private int his_status;
+	private String his_part;
+	
+	public int getHis_num() {
+		return his_num;
+	}
+	public void setHis_num(int his_num) {
+		this.his_num = his_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	public int getSch_num() {
+		return sch_num;
+	}
+	public void setSch_num(int sch_num) {
+		this.sch_num = sch_num;
+	}
+	public int getTra_num() {
+		return tra_num;
+	}
+	public void setTra_num(int tra_num) {
+		this.tra_num = tra_num;
+	}
+	public int getHis_status() {
+		return his_status;
+	}
+	public void setHis_status(int his_status) {
+		this.his_status = his_status;
+	}
+	public String getHis_part() {
+		return his_part;
+	}
+	public void setHis_part(String his_part) {
+		this.his_part = his_part;
+	}
+	
+}
