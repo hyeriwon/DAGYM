2f64d3cd504dc1ce2ce24f2d7b8a73cdf1f4a423
shun04321 <shun04321@gmail.com>
2024-05-24 16:08:27 +0900

    [ADD] 출석체크 servlet

--- DAGYM/src/main/java/kr/attend/action/ListAction.java
+++ DAGYM/src/main/java/kr/attend/action/ListAction.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import kr.attend.dao.AttendDAO;
 import kr.attend.vo.AttendVO;
@@ -15,6 +16,14 @@ public class ListAction implements Action {
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
+//		HttpSession session = request.getSession();
+//		Integer user_num = (Integer)session.getAttribute("user_num");
+//		
+//		if(user_num == null) {//로그인이 되지 않은 경우
+//			return "redirect:/member/loginForm.do";
+//		}
+//		//로그인 된 경우
+		
 		//선택한 페이지 반환
 		String pageNum = request.getParameter("pageNum");
 
