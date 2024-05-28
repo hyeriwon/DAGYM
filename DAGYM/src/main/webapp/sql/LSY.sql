110d561eb56d1ea463ad2573d0239b9fd756e5f8
shun04321 <shun04321@gmail.com>
2024-05-28 11:45:59 +0900

    [ADD] 포인트 테이블

--- DAGYM/src/main/webapp/sql/LSY.sql
+++ DAGYM/src/main/webapp/sql/LSY.sql
@@ -1,3 +1,4 @@
+-- 출석체크
 CREATE TABLE attend (
 	att_num NUMBER NOT NULL,
 	mem_num NUMBER NOT NULL,
@@ -7,3 +8,17 @@ CREATE TABLE attend (
 	CONSTRAINT attend_fk FOREIGN KEY (mem_num) REFERENCES member (mem_num)
 );
 CREATE SEQUENCE attend_seq;
+
+-- 포인트
+CREATE TABLE point (
+	poi_num NUMBER NOT NULL,
+	mem_num NUMBER NOT NULL,
+	poi_type NUMBER(2) NOT NULL,
+	poi_in NUMBER(10),
+	poi_in_date DATE,
+	poi_out NUMBER(10),
+	poi_out_date DATE,
+	CONSTRAINT point_pk PRIMARY KEY (poi_num),
+	CONSTRAINT point_fk FOREIGN KEY (mem_num) REFERENCES member (mem_num)
+);
+CREATE SEQUENCE point_seq;
