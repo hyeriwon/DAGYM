6d1a16885f5242df191b4ecf09e6fd38a827d9c6
HJW <auto05085@gmail.com>
2024-05-30 16:27:40 +0900

    인바디

--- DAGYM/src/main/java/kr/inbody/dao/InbodyDAO.java
+++ DAGYM/src/main/java/kr/inbody/dao/InbodyDAO.java
@@ -59,6 +59,13 @@ public class InbodyDAO {
 			
 			if(rs.next()) {
 				inbody = new InbodyVO();
+				inbody.setInb_hei(rs.getInt("inb_hei"));
+				inbody.setInb_date(rs.getString("inb_date"));
+				inbody.setInb_mus(rs.getInt("inb_mus"));
+				inbody.setInb_num(rs.getInt("inb_num"));
+				inbody.setInb_photo(rs.getString("inb_photo"));
+				inbody.setInb_wei(rs.getInt("inb_wei"));
+				
 			}
 		}catch(Exception e) {
 			throw new Exception(e);
